c04a5fa12b0a15c03465bc040caf81ff37196c2f
Fernando Lozano <flozano@redhat.com>
2025-08-12 18:11:33 +0000

    Minor fixes to the sample web apps

--- bootc-install/virt-install.sh
+++ bootc-install/virt-install.sh
@@ -6,7 +6,7 @@ virt-install \
 --vcpus 2 \
 --ram 4096 \
 --disk size=20 \
---network passt,portForward=8022:22,portForward=8080:80 \
+--network passt,portForward0=8022:22,portForward1=8080:80 \
 --location http://content.example.com/rhel10.0/x86_64/dvd/ \
 --os-variant rhel10.0 \
 --initrd-inject ./inst.ks \
