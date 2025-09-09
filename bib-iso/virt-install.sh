d10b8eb0603f123a8062503c568e79e111e417c5
Fernando Lozano <flozano@redhat.com>
2025-09-09 14:47:02 +0000

    workaround an issue with bib

--- bib-iso/virt-install.sh
+++ bib-iso/virt-install.sh
@@ -5,7 +5,7 @@ virt-install --name bootc-iso \
 --vcpus 2 --memory 4096 \
 --disk size=20 \
 --network passt,portForward0=8022:22,portForward1=8080:80 \
---location httpd-bootc.iso \
+--location httpd-bootc.iso,kernel=/images/pxeboot/vmlinuz,initrd=/images/pxeboot/initrd.img \
 --os-variant rhel10.0 \
 --graphics=none \
 --extra-arg inst.ks=hd:LABEL=$LABEL:/osbuild.ks \
