85206ffd44b9300fb3d278ccd035f087122a8c16
Fernando Lozano <flozano@redhat.com>
2025-09-09 14:12:40 +0000

    gui (workstation gui desktop) installation script

--- bib-iso/virt-install.sh
+++ bib-iso/virt-install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -xeuo pipefail
 
-virt-install \
---name bootc-iso \
---vcpus 2 \
---ram 4096 \
+virt-install --name bootc-iso \
+--vcpus 2 --memory 4096 \
 --disk size=20 \
 --network passt,portForward0=8022:22,portForward1=8080:80 \
 --location httpd-bootc.iso \
 --os-variant rhel10.0 \
---graphics none \
---extra-arg console=ttyS0 \
+--graphics=none \
+--extra-arg inst.ks=hd:LABEL=$LABEL:/osbuild.ks \
+--extra-arg console=ttyS0 -v
