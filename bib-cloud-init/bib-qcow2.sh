159883363d0e872ed9ad9e1eeac6b91c13fd324b
Fernando Lozano <flozano@redhat.com>
2025-09-09 23:43:47 +0000

    incomplete example for using cloud-init

---
+++ bib-cloud-init/bib-qcow2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xeuo pipefail
+
+sudo podman run --rm -it --privileged --pull=newer \
+    --security-opt label=type:unconfined_t \
+    -v /var/lib/containers/storage:/var/lib/containers/storage \
+    -v $(pwd)/config.toml:/config.toml \
+    -v $(pwd)/output:/output \
+    registry.lab.example.com:5000/rhel10/bootc-image-builder \
+    --type qcow2 --local --config /config.toml \
+    registry.lab.example.com:5000/httpd-cloud-init:v1.0
+
---
+++ bib-cloud-init/virt-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -xeuo pipefail
+
+virt-install --name bootc-cloud-init \
+--vcpus 2 --memory 4096 \
+--disk httpd-cloud-init.qcow2,size=20 \
+--network passt,portForward0=8022:22,portForward1=8080:80 \
+--import \
+--os-variant rhel10.0 \
+--graphics=none \
+--cloud-init user-data=user-data
+
+#--cloud-init clouduser-ssh-key=edge-key.pub
+
+# do --extra-arg has any effect?
+#--extra-arg console=ttyS0 -v
