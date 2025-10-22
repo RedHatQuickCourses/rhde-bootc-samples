c89e9fe67409052d813056585ff5abf50f626e6b
Fernando Lozano <flozano@redhat.com>
2025-10-22 13:55:50 +0000

    bootc container for disconnected update lab

---
+++ nginx-bootc/virt-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xeuo pipefail
+
+virt-install --name bootc-nginx \
+--vcpus 2 --memory 4096 \
+--disk size=20 \
+--network passt,portForward0=8022:22,portForward1=8080:80 \
+--location nginx-bootc.iso,kernel=/images/pxeboot/vmlinuz,initrd=/images/pxeboot/initrd.img \
+--os-variant rhel10.0 \
+--graphics=none \
+--extra-arg inst.ks=hd:LABEL=$LABEL:/osbuild.ks \
+--extra-arg console=ttyS0 -v
